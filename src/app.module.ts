9f7469cfe66e6a9344fdacef8ca9983d67d57870
DESKTOP-GR1OQL4\kjm <jinyinshu@nate.com>
2022-10-12 14:15:29 +0900

    asdawqa2

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,13 +54,18 @@ import { UploadsModule } from './uploads/uploads.module';
 
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_HOST,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: true,
-      logging: true,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+      logging:
+        process.env.NODE_ENV !== 'production' &&
+        process.env.NODE_ENV !== 'test',
       entities: [User, Restaurant, Category, Dish, Order, OrderItem],
     }),
     JwtModule.forRoot({
